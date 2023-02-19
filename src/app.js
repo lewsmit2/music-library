608e590653ee23a6ac7e65ea2d21b2fbecf51c9b
Lewis Smith <lewislbssmith212@gmail.com>
2023-02-19 15:27:51 +0000

    sql injection security update

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const app = express();
+const artistRouter = require('./routes/artists');
+
+const app = express(); 
+
 app.use(express.json());
 
-app.get('/', (req, res) => {
-   res.status(200).send('Hello World!');
-});
+app.use('/artists', artistRouter);
 
 module.exports = app;
---
+++ src/routes/artists.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const artistController = require('../controllers/artist');
+const router = express.Router();
+router.post('/', artistController.createArtist);
+
+module.exports = router;

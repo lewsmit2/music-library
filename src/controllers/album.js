f196d0d82d12c673c22b6a71edb557886976f72c
Lewis Smith <lewislbssmith212@gmail.com>
2023-02-27 14:20:29 +0000

    album patch now complete

--- src/controllers/album.js
+++ src/controllers/album.js
@@ -37,4 +37,34 @@ const singleAlbumById = async (req, res) => {
     }
 };
 
-module.exports = {createAlbum, readAlbum, singleAlbumById};
+const patchAlbumById = async (req, res) => {
+    const { id } = req.params;
+    const { name, year } = req.body;
+
+    let query, params;
+
+    if (name && year) {
+        query = `UPDATE Albums SET name = $1, year = $2 WHERE id = $3 RETURNING *`;
+        params = [name, year, id];
+    } else if (name) {
+        query = `UPDATE Albums SET name = $1 WHERE id = $2 RETURNING *`;
+        params = [name, id];
+    } else if (year) {
+        query = `UPDATE Albums SET year = $1 WHERE id = $2 RETURNING *`;
+        params = [year, id];
+    }
+
+    try {
+        const { rows: [ album ] } = await db.query(query, params);
+
+        if(!album) {
+            res.status(404).json({ message: `album ${id} does not exist`});
+        }
+
+        res.status(200).json(album);
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+}
+
+module.exports = {createAlbum, readAlbum, singleAlbumById, patchAlbumById};
--- src/routes/album.js
+++ src/routes/album.js
@@ -3,7 +3,11 @@ const albumsController = require('../controllers/album');
 const router = express.Router();
 
 router.post('/:id/albums', albumsController.createAlbum);
+
 router.get('/', albumsController.readAlbum);
+
 router.get('/:id', albumsController.singleAlbumById);
+
+router.patch('/:id', albumsController.patchAlbumById);
 
 module.exports = router;
